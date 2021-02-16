dc67297c1c98ca9850e5504b2c37f5bc5e2bc630
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2021-02-16 20:46:07 -0300

    sobrescrevendo metodos do ciclo de vida para entender o fucionamento

--- app/src/main/java/com/example/vmodel/MainActivity.kt
+++ app/src/main/java/com/example/vmodel/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.vmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -14,15 +15,48 @@ class MainActivity : AppCompatActivity() {
 
     var contador: Int = 0
 
+    // cria a estrutura de todo o projeto
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        logar(valor = "onCreate")
 
         initDados()
         initContator()
         initClick()
 
         validaContador()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        logar(valor = "onStart")
+    }
+
+    // layout renderizado
+    override fun onResume() {
+        super.onResume()
+        logar(valor = "onResume")
+    }
+
+    override fun onPause() {
+        logar(valor = "onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        logar(valor = "onPause")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        logar(valor = "onDestroy")
+        super.onDestroy()
+    }
+
+    private fun logar(tag: String = "Ciclo de Vida", valor: String) {
+        Log.d(tag, valor)
     }
 
     private fun validaContador() {
